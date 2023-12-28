a957f3e40fd21dc7674ed6c74353367d58fc6319
Erol SARICA <erol.sarica@boun.edu.tr>
2023-12-28 23:59:31 +0300

    Add styling to share button

--- lib/share_button.dart
+++ lib/share_button.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 shareButton(String title, Function()? onPressed) {
   return ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      primary: Colors.blue,
+      onPrimary: Colors.white,
+      shape: const StadiumBorder(),
+    ),  
     onPressed: () {},
     child: Text(
       title,
